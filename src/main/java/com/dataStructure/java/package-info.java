0923b7c2d799f04565614f6aeda1e46e9ea7f6ba
gets-mukul <mukulpraj9@gmail.com>
2019-01-20 00:03:05 +0530

    intital commit

---
+++ src/main/java/com/dataStructure/java/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author mukul
+ *
+ */
+package com.dataStructure.java;
+//
+//THis package include all the data mplementatini of data structuresi java 
+//mainlly focused in tree nad graph
