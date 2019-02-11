f2049f2c1099c3ec3f74657640cda8552a80d0fc
gets-mukul <mukulpraj9@gmail.com>
2019-02-11 10:28:21 +0530

    roboit in a grid

--- src/main/java/com/dataStructure/java/RobotInAGrid.java
+++ src/main/java/com/dataStructure/java/RobotInAGrid.java
@@ -8,34 +8,54 @@ package com.dataStructure.java;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Point {
+	int row;
+	int col;
+
+	Point(int row, int col) {
+		row = row;
+		col = col;
+	}
+}
+
 public class RobotInAGrid {
-	public static void checkForPath(int i, int j, ArrayList<String> path, int grid[][]) {
-		if (i == 0 && j == 0) {
-			return;
-		} else if (grid[i][j] != -1) {
-			String str1 = Integer.toString(i);
-			String str2 = Integer.toString(j);
-			path.add(str1 + str2);
-		}
-		checkForPath(i - 1, j, path, grid);
-		checkForPath(i, j - 1, path, grid);
 
-		for (String x : path) {
-			System.out.println(x);
+	public static boolean checkForPath(int i, int j, ArrayList<Point> path, boolean[][] grid) {
+
+		if (i < 0 || j < 0 || !grid[i][j]) {
+			return false;
 		}
 
+		else if (checkForPath(i, j - 1, path, grid) || checkForPath(i - 1, j, path, grid)) {
+			Point point = new Point(i, j);
+			path.add(point);
+			return true;
+		}
+		return false;
+	}
+
+	public static ArrayList<Point> checkForPath(boolean[][] grid) {
+		if (grid == null || grid.length == 0) {
+			return null;
+		}
+		ArrayList<Point> path = new ArrayList<Point>();
+		if (checkForPath(grid.length - 1, grid[0].length - 1, path, grid)) {
+			System.out.println("---------------------");
+			return path;
+		}
+		return null;
 	}
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		boolean[][] grid;
 
 		System.out.print("Enter the number of rows and columns for a grid: ");
 		int r = sc.nextInt();
 		int c = sc.nextInt();
 
-		ArrayList<String> path = new ArrayList<>();
-		int grid[][] = new int[r][c];
+		grid = new boolean[r][c];
 
 		System.out.println("Enter number of off-set cell:");
 		int n = sc.nextInt();
@@ -45,13 +65,21 @@ public class RobotInAGrid {
 			for (int i = 1; i <= n; i++) {
 				int row = sc.nextInt();
 				int col = sc.nextInt();
-				grid[row][col] = -1;
+				grid[row][col] = true;
 
 			}
 		}
 
-		checkForPath(r - 1, c - 1, path, grid);
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				System.out.print(grid[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		ArrayList<Point> path = checkForPath(grid);
+
+		System.out.println(path);
 
 	}
-
 }
