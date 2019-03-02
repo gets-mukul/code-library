483065aa872e48b6029d9e2bcb71c2f3a1d24a96
gets-mukul <mukulpraj9@gmail.com>
2019-03-02 19:14:41 +0530

    nnn

--- src/main/java/com/dataStructure/java/Test.java
+++ src/main/java/com/dataStructure/java/Test.java
@@ -1,30 +1,11 @@
 package com.dataStructure.java;
 
 public class Test {
-	
-	public static void getAlternateValue(int[] arr, int index) {
-		//check if the value of index is out of arraybound 
-		
-		if(index >= arr.length) {
-			//if index is out of bound then return
-			return;
-		}
-		
-		//print the current value of index
-		System.out.println(arr[index]);
-		
-		//recursive call for a alternate index
-		getAlternateValue(arr, index + 2);
-	}
-
 	public static void main(String[] args) {
+		String str = "a";
+		String str2 = "ab";
 		
-		// int array with 5 elements
-		int[] arr = new int[] {1,2,3,4,5};
-		
-		//method which print alternate values from array starts with 0th index
-		getAlternateValue(arr, 0);
-
+		System.out.println(str2.compareTo(str));
 	}
 
 }
